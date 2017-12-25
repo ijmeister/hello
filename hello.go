675f608d4634437627be9230481d00438c610705
Raymond Aung <raymond.psaung@gmail.com>
2017-12-25 16:58:00 +0630

    first lib

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+// import "fmt"
+import (
+    "fmt"
+    "github.com/ijmeister/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world.\n")
+    // fmt.Printf("hello, world.\n")
+    fmt.Printf(stringutil.Reverse("\n!oG, olleH"))
 }
